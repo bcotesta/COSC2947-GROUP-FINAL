92e2bddf88c0345e9b1882c10777d0dcaf124ac2
bcotesta <bcotesta@gmail.com>
2025-10-09 21:15:12 -0400

    program working. remeber to reference header files not cpp

--- source/main.cpp
+++ source/main.cpp
@@ -9,15 +9,20 @@ int main()
 {
 	cout << "-- GREATER SUDBURY BANKING SERVICE --" << endl;
 	string currentInput;
+	Account currentAccount("123456789", AccountType::CHEQUING);
+	currentAccount.deposit(1000.0); // Initial deposit for demonstration
 	bool running = true;
 	while(running)
 	{
 		cout << "Enter command (help for list of commands): ";
-		getline(cin, currentInput);
+		getline(cin >> ws, currentInput);
 		if(currentInput == "help")
 		{
 			cout << "Available commands:" << endl;
 			cout << " help - Show this help message" << endl;
+			cout << " view balance - Show selected account balance" << endl;
+			cout << " deposit - Deposit funds into selected account" << endl;
+			cout << " withdraw - Withdraw funds from selected account" << endl;
 			cout << " exit - Exit the application" << endl;
 			// Add more commands as needed
 		}
@@ -25,6 +30,29 @@ int main()
 		{
 			running = false;
 			cout << "Exiting the application. Goodbye!" << endl;
+		}
+		else if(currentInput == "view balance")
+		{
+			// Placeholder for viewing balance logic
+			cout << "Account balance: $" << currentAccount.getBalance() << endl;
+		}
+		else if (currentInput == "deposit") {
+			cout << "How much would you like to deposit? ";
+			int amnt = 0;
+			cin >> amnt;
+			currentAccount.deposit(amnt);
+		}
+		else if (currentInput == "withdraw")
+		{
+			// Placeholder for withdrawal logic
+			cout << "How much would you like to withdraw? ";
+			int amnt = 0;
+			cin >> amnt;
+			currentAccount.withdraw(amnt);
+		}
+		else if (currentInput == "transfer funds")
+		{
+			// Placeholder for transferring funds}
 		}
 		else
 		{
