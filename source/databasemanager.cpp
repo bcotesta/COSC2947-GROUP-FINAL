b02e89fa7c0b2058eaf376d1b6fb6ec08fd2bbff
bcotesta <bcotesta@gmail.com>
2025-10-17 17:07:45 -0400

    refactor

--- source/databasemanager.cpp
+++ source/databasemanager.cpp
@@ -1,80 +1,91 @@
 //Everest Ashley SID: 0457240
+// REFACTOR Brandon Cotesta 10/20/2025
 #include "databasemanager.h"
 #include <iostream>
 
-
-databasemanager::databasemanager():
+databasemanager::databasemanager()
 {
-  //establish connection 
-  sql::mysql::MySQL_Driver* driver;
-  sql::Connection* con;
+    //establish connection 
+    sql::mysql::MySQL_Driver* driver;
+    sql::Connection* con;
+    stmt = nullptr;
+    connection = nullptr;
 
-try {
-    int tempint = 0;
-    driver = sql::mysql::get_driver_instance();
-    con = driver->connect("tcp://127.0.0.1:3306", "root", "keypick1"); 
-  // connect to driver instance
-  
-    con->setSchema("bankdatabase"); 
-   //Database that holds all tables
+    try {
+        driver = sql::mysql::get_driver_instance();
+        con = driver->connect("tcp://127.0.0.1:3306", "root", "keypick1");
+        // connect to driver instance
+
+        con->setSchema("bankdatabase");
+        //Database that holds all tables
+        
+        stmt = con->createStatement();
+        connection = con; // Store connection for later use
     }
-  catch (sql::SQLException &e){ //catch for errors
-    std::cerr << "SQL Error: " << e.what() << std::endl;
+    catch (sql::SQLException& e) { //catch for errors
+        std::cerr << "SQL Error: " << e.what() << std::endl;
+    }
 }
 
-  void databasemanager::addtoTable(string tab, string val)
-  {
-    string statement = this.insert + t + " " + this.values + val; 
+void databasemanager::addtoTable(std::string tab, std::string val)
+{
+    std::string statement = insert + tab + " " + values + val;
     //create statement to execute
-    
-     sql::ResultSet* res = stmt->executeQuery(statement); 
-    //execute query 
-    
-  }
 
-string databasemanager::retString(string col, string tab, string specval)
-{
-  std::string tempstring; 
-  //string that will be returned;
-  
-  this.statement = this.select + col + " " + this.from + t; 
-  //uses the statement variable to make a string statement for execution
-  
-  sql::ResultSet* res = stmt->executeQuery(this.statement); 
-  //executes the statement assigns value to res (result set)
-  
-  tempstring = res->getstring(specval); 
-  //searches returned statement for the specific value wanted.
-
-  delete res;  //clean up otherwise it can cause some issues with multiple res and stmt
-  delete stmt;
-  
-  return tempstring;
+    stmt->execute(statement); 
+    //execute query - use execute() for INSERT statements
 }
 
-string databasemanager::retStringW(string col, string tab, string val, string specval)
+std::string databasemanager::retString(std::string col, std::string tab, std::string specval)
+{
+    std::string tempstring; 
+    //string that will be returned;
+  
+    statement = select + col + " " + from + tab; 
+    //uses the statement variable to make a string statement for execution
+  
+    sql::ResultSet* res = stmt->executeQuery(statement); 
+    //executes the statement assigns value to res (result set)
+  
+    if (res->next()) {
+        tempstring = res->getString(specval); 
+        //searches returned statement for the specific value wanted.
+    }
+
+    delete res;  //clean up result set only
+    // Don't delete stmt - it's a class member
+  
+    return tempstring;
+}
+
+std::string databasemanager::retStringW(std::string col, std::string tab, std::string val, std::string specval)
 { //select statement with where 
-  std::string tempstring; 
-  //same comments and functions as retString
-  this.statement = this.select + col + " " + this.from + tab + " " + this.where + val ; 
-  //except here it uses the WHERE statement to give a more specific search
+    std::string tempstring; 
+    //same comments and functions as retString
+    statement = select + col + " " + from + tab + " " + where + val; 
+    //except here it uses the WHERE statement to give a more specific search
   
-  sql::ResultSet* res = stmt->executeQuery(this.statement);
-  tempstring = res->getstring(specval);
+    sql::ResultSet* res = stmt->executeQuery(statement);
+    
+    if (res->next()) {
+        tempstring = res->getString(specval);
+    }
 
-  delete res;  //clean up 
-  delete stmt;
+    delete res;  //clean up result set only
+    // Don't delete stmt - it's a class member
   
-  return tempstring;
+    return tempstring;
 }
 
-void databasemanager::updateTable(string tab, string setv, string cond)
+// tab = table, setv = set values, cond = condition
+void databasemanager::updateTable(std::string tab, std::string setv, std::string cond)
 {
-  this.statement = this.update + tab + " " + this.set + setv + " " + this.where + cond; 
-  //create statement for execution with UPDATE, SET, and WHERE
-  //example UPDATE userinfo SET email = 'testemail' name = 'testname' WHERE userID = 1;
-  //will update values for userID 1
-  sql::ResultSet* res = stmt->executeQuery(this.statement);
-  //Executes statement
-  //no return needed
+    statement = update + tab + " " + set + setv + " " + where + cond; 
+    //create statement for execution with UPDATE, SET, and WHERE
+    //example UPDATE userinfo SET email = 'testemail' name = 'testname' WHERE userID = 1;
+    //will update values for userID 1
+    
+    stmt->execute(statement);
+    //Executes statement - use execute() for UPDATE statements
+    //no return needed
 }
--- source/databasemanager.h
+++ source/databasemanager.h
@@ -1,4 +1,5 @@
 //Everest Ashley SID: 0457240
+// REFACTOR Brandon Cotesta 10/20/2025
 
 #include <iostream>
 #include <mysqlx/xdevapi.h>
@@ -9,43 +10,41 @@
 #include <jdbc/cppconn/statement.h>
 #include <string>
 
-
-using ::std::cout;
-using ::std::endl;
+using std::string;
+using namespace std;
 using namespace ::mysqlx;
 
-
-class UserInfo
+class databasemanager
 {
 public:
-  databasemanager(); //connect to database
-
+	databasemanager(); //connect to database
 
 //functions
-  void addtoTable(string tab, string val);
-  // (t) table to add a row to, (v) values to insert in ('value1','value2') format 
-  // or "('"+ name + "', '" + email + "')" format with variables
+	void addtoTable(std::string tab, std::string val);
+	// (t) table to add a row to, (v) values to insert in ('value1','value2') format 
+	// or "('"+ name + "', '" + email + "')" format with variables
 
-  string retString(string col, string tab, string specval); 
-  // select statement without where statement
+	std::string retString(std::string col, std::string tab, std::string specval);
+	// select statement without where statement
   
-  string retStringW(string col, string tab, string val, string specval); 
-  // (c)column/s to select, (t)table to select from, (v)value to select if using where 
-  // ex select + c + from + t + where + v + ";";
+	std::string retStringW(std::string col, std::string tab, std::string val, std::string specval);
+	// (c)column/s to select, (t)table to select from, (v)value to select if using where 
+	// ex select + c + from + t + where + v + ";";
 
-  void updateTable(string tab, string setv + string cond);
-  // ex string state = update + t + set + setv + where + cond + ";"
+	void updateTable(std::string tab, std::string setv, std::string cond);
+	// ex string state = update + t + set + setv + where + cond + ";"
 
 private:
-  string statement;
-  string table;
+	std::string statement;
+	std::string table;
+	sql::Statement* stmt;        // Added missing member
+	sql::Connection* connection; // Added missing member
 // values for easier sql queries
-  string insert = "INSERT INTO ";
-  string values = "VALUES ";
-  string update =  "UPDATE ";
-  string set = "SET ";
-  string select = "SELECT ";
-  string from = "FROM ";
-  string where = "WHERE ";
-  
+	std::string insert = "INSERT INTO ";
+	std::string values = "VALUES ";
+	std::string update =  "UPDATE ";
+	std::string set = "SET ";
+	std::string select = "SELECT ";
+	std::string from = "FROM ";
+	std::string where = "WHERE ";
 };
