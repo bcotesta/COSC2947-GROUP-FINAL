76a703ff29610dbbe259c449664ff199968403db
eashley532 <eashley1@laurentian.ca>
2025-10-17 12:58:26 -0400

    Create databasemanager.h
    class for data base management

---
+++ databasemanager.h
@@ -0,0 +1,29 @@
+//Everest Ashley SID: 0457240
+
+#include <iostream>
+#include <mysqlx/xdevapi.h>
+#include <mysql/jdbc.h>
+#include <jdbc/mysql_driver.h>
+#include <jdbc/mysql_connection.h>
+#include <jdbc/cppconn/resultset.h>
+#include <jdbc/cppconn/statement.h>
+#include <string>
+
+
+using ::std::cout;
+using ::std::endl;
+using namespace ::mysqlx;
+
+
+class UserInfo
+{
+public:
+  databasemanager(string t, string st) const; //do statement st with table t
+  
+
+private:
+  string statement;
+  string table;
+  
+  
+};
